2e63419fba816fffec481f288bccacfe1f828538
Ammar Masud <ammarmasud@gmail.com>
2017-03-14 23:13:37 +0000

    done

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,10 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts 'Insert store name:'
+print '> '
+@store_name = gets.chomp
+
+@store_new = Store.new(name: @store_name)
+@store_new.valid?
+puts @store_new.errors.messages
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
+  has_many :employees
 
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {greater_than_or_equal_to: 0}
+  validate :has_apparel
+
+  def has_apparel
+    if !mens_apparel && !womens_apparel
+      errors.add(:name, "doesn't sell anything")
+    end
+  end
 end
